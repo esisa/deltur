f2e2b042fe093a7c3880146f89c237820d440be1
Espen Oldeman Lund <espen@espenpost.com>
2014-03-20 10:41:06 +0100

    #28 Beskjed til IE9 bruker om gammel nettleser

---
+++ app/delturapp/static/js/fileInputCheck.js
@@ -0,0 +1,25 @@
+// Check if browser supports fileinput
+$(function(){
+    var message = '<p>Du bruker dessverre en gammel nettleser som ikke støtter mange av dagens funksjoner på internett.</p>'
+    message += '<p>Det fører til at du ikke kan laste opp GPS-spor på deltur.no<p> '
+    message += '<p>deltur.no anbefaler deg å oppgradere din nettleser før du deler turen din. <p> '
+
+    var elem = document.createElement('input');
+    elem.type = 'file';
+    if(!elem.disabled) {
+
+        BootstrapDialog.show({
+            title: 'Beklager!',
+            cssClass: 'ie9-dialog',
+            message: message,
+            buttons: [{
+                label: 'OK',
+                action: function(dialogref){
+                    dialogref.close();
+                }
+            }]
+        }); 
+
+        
+    } 
+});
